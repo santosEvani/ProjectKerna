bfb624359722d24b44484095ae878682ef193e73
Said <karolaynedias12@gmail.com>
2018-05-30 18:00:47 +0000

    Controllers da pagina main

---
+++ CI/application/controllers/Pagemain.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+//rota: https://kerna-karolaynedias.c9users.io/CI/index.php/pagemain/index
+class Pagemain extends CI_Controller {
+
+	public function index(){
+		$this->load->helper('url'); 
+		$this->load->view('home');
+	}
+}
