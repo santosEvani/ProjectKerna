f10afa558aba548614e9de0eb12fd3978d93d56b
Said <karolaynedias12@gmail.com>
2018-06-02 04:09:17 +0000

    Configuração de protocolos de email

---
+++ CI/application/config/email.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['protocol']  = 'smtp'; 
+$config['charset'] = 'utf-8';
+$config['wordwrap'] = TRUE;
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
+$config['smtp_user'] = 'karolaynedias12@gmail.com';
+$config['smtp_port'] = "587";
+$config['newline']  = "\r\n";
+$config['crlf']     = "\r\n";
+
+
+
+
