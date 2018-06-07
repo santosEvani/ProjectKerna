cc842aeb55cd9f9ddfa2d7cb15343f14282708dc
Said <karolaynedias12@gmail.com>
2018-06-07 02:50:03 +0000

    Continuação da construção - validação USER

--- CI/application/controllers/Cadastro.php
+++ CI/application/controllers/Cadastro.php
@@ -15,34 +15,53 @@ class Cadastro extends CI_Controller {
 	public function page_cadastro(){
 		$this->load->view('cadastro');
 	}
-<<<<<<< HEAD
 	
+public function validarSenha(){
+        
+        $senha = $this->input->post('senha');
+        
+        if (!preg_match("/^[a-zA-Z0-9]*$/", $senha)) {
+            
+            $erros['msgErros'] = "Somente letras e números são permitidos.";
+            
+        } else {
+            return TRUE;
+        }
+        
+       $this->load->view('cadastro', $erros);
+    }
 
 	
 	public function validarCampos(){
+	    
+	    if($this->validarSenha() == TRUE){ 
         
-        //seta as regras de validacao dos campos
-        $this->form_validation->set_rules('username', 'Username', 'required|min_length[3]|max_length[50]');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|max_length[64]');
-        $this->form_validation->set_rules('senha', 'Senha', 'required|trim|min_length[10]|max_length[10]');
-        $this->form_validation->set_rules('confimarSenha', 'Confimar Senha', 'required|matches[senha]|trim|min_length[10]|max_length[10]');
-        
-        $this->validarSenha();
-
-        if ($this->form_validation->run() == FALSE){
-        	
-        	$erros['msgErros'] = validation_errors();
-            $this->load->view('cadastro', $erros);
+            //seta as regras de validacao dos campos
+                $this->form_validation->set_rules('username', 'Username', 'required|min_length[3]|max_length[50]');
+                $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|max_length[64]');
+                $this->form_validation->set_rules('senha', 'Senha', 'required|trim|min_length[10]|max_length[10]');
+                $this->form_validation->set_rules('confimarSenha', 'Confimar Senha', 'required|matches[senha]|trim');
+               
+                if ($this->form_validation->run() == FALSE){
+                	$erros['msgErros'] = validation_errors();
+                    $this->load->view('cadastro', $erros);
+                    
+                } else {
+                    $enviado['enviado'] = "Cadastro realizado com sucesso.!!!";
+                    $this->load->view('cadastro', $enviado);
+                }
             
         } else {
-            
-            echo "Cadastro realizado com sucesso.!!!";
-       
+            $erros['msgErros'] = validation_errors();
+            $this->load->view('cadastro',$erros);
         }
+        
+        
+                
+	}
       
         
-=======
-	 public function inserir(){
+	/* public function inserir(){
         require_once APPPATH."/models/NovoUsuario.php";
         $nome = $this->input->post("nome");
         $email = $this->input->post("email");
@@ -55,6 +74,6 @@ class Cadastro extends CI_Controller {
         }else{
             $this->load->view("erro");
         }
->>>>>>> ab134c3af4a7ede3e50f3ff626070e28ce74581a
-    }
+
+    }*/
 }
