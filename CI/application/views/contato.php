c64e338cf5dbc3f18ec577907f50ec9e86fe2fa1
Said <karolaynedias12@gmail.com>
2018-06-07 02:45:21 +0000

    Modificações na exibição da página

--- CI/application/views/contato.php
+++ CI/application/views/contato.php
@@ -6,7 +6,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>Contato - Kerna</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/contato.css');?>">
-	<script type="text/javascript" src="<?= base_url('public/js/js.js');?>"></script>
+	<script type="text/javascript" src="<?= base_url('public/js/js.js');?>">
+		document.form.reset();
+	</script>
 </head>
 <body>
 	<?php $this->load->view('menuSupresa');?>
@@ -20,39 +22,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			
 		</div>
 		
-		<div>
 		<?php 
 			// serve para saber se uma variavel existe
-			if(isset($msgErros)){
-				echo $msgErros;
+		if(isset($msgErros)){
+			echo '<div style="color:#FF4500;font-size:0.9em;margin-top:3%;text-align: center;">'. $msgErros .'</div>';
+		}
+		?>
+		
+		<?php
+		if(isset($enviadoSucesso)){
+			
+			echo '<div style="color:green;font-size:1em;margin-top:3%;text-align: center;">'. $enviadoSucesso .'</div>';
 			}
 		?>
-		</div>
 		
 		<form id="form" name="form" action="<?= site_url('contato/enviaEmail');?>" method="post" accept-charset="utf-8">
 			<p>
 				<label for="name">Digite seu nome </label><br/>
-	        	<input id="name" name="name" type="text" size="50"/>
+	        	<input id="name" name="name" type="text" value="<?= set_value('name'); ?>" size="50"/>
 			</p>
 
 			<p>
 				<label for="email">Digite seu email </label><br/>
-	       		<input id="email" name="email" type="email" size="50"/>
+	       		<input id="email" name="email" type="email" value="<?= set_value('email'); ?>" size="50"/>
 			</p>
 
 			<p>
 				<label for="assunto">Assunto </label><br/>
-	        	<input id="assunto" name="assunto" type="text" size="50"/>
+	        	<input id="assunto" name="assunto" type="text" value="<?= set_value('assunto'); ?>" size="50"/>
 			</p>
 
 			<p>
 				<label for="mensagem">Mensagem </label><br/>
-				<textarea id="mensagem" name="mensagem" rows="8" cols="80"></textarea>
+				<textarea id="mensagem" name="mensagem" rows="8" cols="80" value="<?= set_value('mensagem'); ?>" ></textarea>
 			</p>
 			
 			<button class="butCad btnModal" type="submit" form="form" value="submit" id="enviar" onclick="validar()">Enviar</button>
 
 		</form>
+		
 
 	</div>
 
